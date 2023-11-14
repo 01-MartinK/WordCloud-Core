e460f3f8ccfabce068fa1b6cf77cb4dc23126169
MK <46935118+TheDeadGamer101@users.noreply.github.com>
2023-11-14 21:57:34 +0200

    RabbitMQ integration tests

--- src/main/java/com/component/core/WordcloudCoreComponentApplication.java
+++ src/main/java/com/component/core/WordcloudCoreComponentApplication.java
@@ -1,10 +1,53 @@
 package com.component.core;
 
+import com.component.core.mq.Receiver;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WordcloudCoreComponentApplication {
+
+	static final String topicExchangeName = "wordcloud";
+	static final String queueName = "spring-boot";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("word.cloud.#");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WordcloudCoreComponentApplication.class, args);
